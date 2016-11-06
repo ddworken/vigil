d856dde473ad8b11a3dfa63b13508ceac4ad18af
David Dworken <david@daviddworken.com>
2016-11-05 23:57:13 -0400

    Fixed readme

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+import pypandoc
 
 setup(
     name='vigil',
-    version='0.1.1',
+    version='0.1.2',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['vigil'],
     url='http://pypi.python.org/pypi/vigil/',
     license='LICENSE.txt',
     description='vigil... as a python package',
-    long_description=open('README.md').read(),
+    long_description=pypandoc.convert("README.md", "rst"),
 )
